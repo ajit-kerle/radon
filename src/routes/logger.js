8400b90ec26f226eb78c3caea442ce03d990f465
ajit kerle <ajitkerle70@gmail.com>
2022-06-01 16:19:00 +0530

    node js introduction code

---
+++ src/routes/logger.js
@@ -0,0 +1,9 @@
+const endpoit="https://www.functionup.org"
+const batch="radon"
+const log=function(){
+    console.log("i am inside the log function")
+}
+
+module.exports.url=endpoit
+module.exports.cohort=batch
+module.exports.log=log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const logger= require('./logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    console.log("the constant has value "+logger.url)
+    console.log("the second constant has value "+logger.cohort)
+    logger.log()
+    res.send('welcome world')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('welcome world1')
+});
+router.get('/test-me2', function (req, res) {
+    res.send('welcome world2')
+});
+router.get('/test-me3', function (req, res) {
+    res.send('welcome world3')
 });
 
 module.exports = router;
